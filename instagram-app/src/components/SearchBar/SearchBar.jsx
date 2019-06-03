393664e374e5c42f378110de067ad76d97bde3b4
Ikechukwu Eze <iykekings36@gmail.com>
2019-06-03 18:41:23 +0100

    Integrated Icon component

---
+++ instagram-app/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './SearchBar.css';
+import logo from '../../assets/logo.png';
+import Icon from '../Icons/Icon';
+
+const SearchBar = () => {
+  return (
+    <div className="search-bar">
+      <div className="s-logo">
+        <Icon icon="instagram" fill="#2a2a2a" />
+        <div className="img-logo">
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
+      <div className="spacer" />
+      <div className="s-input">
+        <div className="input-bg">
+          <Icon icon="search" fill="#2a2a2a" />
+          <span>Search</span>
+        </div>
+        <div className="input-main">
+          <form>
+            <input type="text" name="search" id="search" />
+          </form>
+        </div>
+      </div>
+      <div className="spacer" />
+      <div className="s-icons">
+        <Icon icon="compass" fill="#2a2a2a" />
+        <Icon icon="heart" stroke="#2a2a2a" strokeWidth="15px" />
+        <Icon icon="user" fill="#2a2a2a" />
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
