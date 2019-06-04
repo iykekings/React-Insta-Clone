78d44e6a5675b83117077a2378226fad749485f4
Ikechukwu Eze <iykekings36@gmail.com>
2019-06-04 04:24:58 +0100

    Add liked and id field to dummy-data.js

--- instagram-app/src/assets/dummy-data.js
+++ instagram-app/src/assets/dummy-data.js
@@ -1,5 +1,7 @@
 const dummyData = [
   {
+    id: '12345',
+    liked: true,
     username: "philzcoffee",
     thumbnailUrl:
       "https://tk-assets.lambdaschool.com/ecd33d34-c124-4b75-92d2-e5c52c171ed8_11201517_887808411287357_1307163552_a.jpg",
@@ -25,7 +27,9 @@ const dummyData = [
     ]
   },
   {
+    id: '12346',
     username: "fortnite",
+    liked: false,
     thumbnailUrl:
       "https://tk-assets.lambdaschool.com/ce601fdf-7cb0-4098-83d3-1a1584a72513_30841289_342445456281079_112845064497004544_n.jpg",
     imageUrl:
@@ -56,6 +60,8 @@ const dummyData = [
     ]
   },
   {
+    id: '12347',
+    liked: true,
     username: "playhearthstone",
     thumbnailUrl:
       "https://tk-assets.lambdaschool.com/c432f179-8bd7-4758-959d-d88a21f96bca_37105899_432228420518610_5035444036064772096_n.jpg",
