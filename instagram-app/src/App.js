75b4e61050cbf2657c9ac99b2e58f6253aa32e66
Ikechukwu Eze <iykekings36@gmail.com>
2019-06-04 13:16:55 +0100

    rewrite app component with class

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,50 +1,101 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import SearchBar from './components/SearchBar/SearchBar';
 import dummyData from './assets/dummy-data';
 import PostContainer from './components/PostContainer/PostContainer';
+export default class App extends Component{
+  constructor() {
+    super();
+    this.state = {
+      posts: []
+    }
+  }
 
-function App() {
-  const [posts, setPosts] = useState([...dummyData]);
+  componentDidMount() {
+    this.setState({posts: [...dummyData]});
+  }
 
-  const toggleLike = id => {
-    const newPosts = posts.map(post => {
+  toggleLike = id => {
+    const newPosts = this.state.posts.map(post => {
       if (post.id === id) {
         post.liked = !post.liked;
       }
       return post;
     });
-    setPosts([...newPosts]);
+  this.setState({posts: [...newPosts]});
   };
 
-  const addComment = (e, id) => {
+  addComment = (e, id) => {
     e.preventDefault();
     let input = e.target.querySelector('input[type="text"]');
-    const newPosts = posts.map(post => {
+    const newPosts = this.state.posts.map(post => {
       if (post.id === id && !!input.value) {
         post.comments.push({ username: 'User', text: input.value });
       }
       return post;
     });
-    setPosts([...newPosts]);
+    this.setState({posts: [...newPosts]});
     input.value = '';
   };
-
-  return (
+  render() {
+    return (
     <div className="App">
       <SearchBar />
       <section className="all-posts">
-        {posts.map(post => (
+        {this.state.posts.map(post => (
           <PostContainer
             key={post.username}
             {...post}
-            addComment={(e, id) => addComment(e, post.id)}
-            toggleLike={() => toggleLike(post.id)}
+            addComment={(e, id) => this.addComment(e, post.id)}
+            toggleLike={() => this.toggleLike(post.id)}
           />
         ))}
       </section>
     </div>
   );
+  }
 }
+// function Appfunc() {
+//   // const [posts, setPosts] = useState([...dummyData]);
 
-export default App;
+//   const toggleLike = id => {
+//     const newPosts = posts.map(post => {
+//       if (post.id === id) {
+//         post.liked = !post.liked;
+//       }
+//       return post;
+//     });
+//     setPosts([...newPosts]);
+//   };
+
+//   const addComment = (e, id) => {
+//     e.preventDefault();
+//     let input = e.target.querySelector('input[type="text"]');
+//     const newPosts = posts.map(post => {
+//       if (post.id === id && !!input.value) {
+//         post.comments.push({ username: 'User', text: input.value });
+//       }
+//       return post;
+//     });
+//     setPosts([...newPosts]);
+//     input.value = '';
+//   };
+
+//   return (
+//     <div className="App">
+//       <SearchBar />
+//       <section className="all-posts">
+//         {posts.map(post => (
+//           <PostContainer
+//             key={post.username}
+//             {...post}
+//             addComment={(e, id) => addComment(e, post.id)}
+//             toggleLike={() => toggleLike(post.id)}
+//           />
+//         ))}
+//       </section>
+//     </div>
+//   );
+// }
+
+// export default App;
