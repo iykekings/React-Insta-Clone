48daf7ecaeec6d4c4a7a1c9c0f6ce8fe65c3e8d5
Ikechukwu Eze <iykekings36@gmail.com>
2019-06-06 15:11:40 +0100

    Refactored SearchBar logo and Spacer

--- instagram-app/src/components/Icons/Icon.jsx
+++ instagram-app/src/components/Icons/Icon.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// import './Icon.css';
 import IconsPaths from './IconPaths';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
--- instagram-app/src/components/SearchBar/SearchBar.jsx
+++ instagram-app/src/components/SearchBar/SearchBar.jsx
@@ -3,29 +3,57 @@ import PropTypes from 'prop-types';
 import './SearchBar.css';
 import logo from '../../assets/logo.png';
 import Icon from '../Icons/Icon';
+import styled from 'styled-components';
 
-const SearchBar = ({runSearch, searchValue, handleSearchInput}) => {
+const ImgDiv = styled.div`
+  width: 12rem;
+  display: flex;
+  align-items: center;
+  img {
+    width: 80%;
+  }
+  &:before {
+    content: '';
+    background: #b7b6b6;
+    height: 40px;
+    width: 1px;
+    margin-right: 12px;
+    margin-left: 12px;
+  }
+`;
+
+const Spacer = styled.div`
+  flex: 1;
+`;
+
+const SearchBar = ({ runSearch, searchValue, handleSearchInput }) => {
   return (
     <div className="search-bar">
       <div className="s-logo">
-        <Icon icon="instagram" fill="#2a2a2a" />
-        <div className="img-logo">
+        <Icon icon="instagram" fill="#2a2a2a" width="40%" padding="0.7rem" />
+        <ImgDiv>
           <img src={logo} alt="logo" />
-        </div>
+        </ImgDiv>
       </div>
-      <div className="spacer" />
+      <Spacer />
       <div className="s-input">
         <div className="input-bg">
           <Icon icon="search" fill="grey" />
           <span>Search</span>
         </div>
         <div className="input-main">
-          <form onSubmit={(e) => runSearch(e)}>
-            <input type="text" name="search" onChange={(e) => handleSearchInput(e.target.value)} id="search" value={searchValue}/>
+          <form onSubmit={e => runSearch(e)}>
+            <input
+              type="text"
+              name="search"
+              onChange={e => handleSearchInput(e.target.value)}
+              id="search"
+              value={searchValue}
+            />
           </form>
         </div>
       </div>
-      <div className="spacer" />
+      <Spacer />
       <div className="s-icons">
         <Icon icon="compass" fill="#2a2a2a" />
         <Icon icon="heart" fill="#2a2a2a" />
@@ -39,5 +67,5 @@ SearchBar.propTypes = {
   runSearch: PropTypes.func.isRequired,
   searchValue: PropTypes.string,
   handleSearchInput: PropTypes.func.isRequired
-}
+};
 export default SearchBar;
